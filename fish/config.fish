7658dc3fee3eeda013ac5b307ebaab500c457fa5
Thubie de Jong <thubie@Thubies-MacBook-Pro.local>
2024-01-15 21:35:23 +0100

    Modified helix, wezterm and fish config files

--- fish/config.fish
+++ fish/config.fish
@@ -1,10 +1,22 @@
 fish_add_path "opt/homebrew/bin/"
 
 alias ga="git add"
+alias gc="git commit"
+alias gs="git status"
+alias gbn="git new branch"
+
+
 function ga 
     git add $argv;
 end
 
+function gc
+    git commit -m $argv;
+end
+
+function gs
+    git status $argv;
+end
 
 starship init fish | source
 zoxide init fish | source
