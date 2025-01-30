b3d770c47b7da92149620f707317b6fb5948f10a
Thubie de Jong <thubie@Thubies-MacBook-Pro.local>
2025-01-30 19:47:02 +0100

    Configured ghostty, helix and zellij

--- fish/config.fish
+++ fish/config.fish
@@ -3,16 +3,20 @@ zoxide init fish | source
 
 fish_add_path opt/homebrew/bin/
 fish_add_path ~/tools/zls/zig-out/bin
+fish_add_path ~/tools/omnisharp
 fish_add_path ~/tools/zigup/zig-out/bin
 fish_add_path ~/tools/bin
 fish_add_path ~/tools/wgsl-analyzer/target/release
-fish_add_path ~/VulkanSDK/1.3.283.0/macOS/bin
+fish_add_path /Users/thubie/VulkanSDK/1.3.296.0/macOS/bin
+fish_add_path /opt/homebrew/opt/llvm/bin
 
+set -Ux VULKAN_SDK /Users/thubie/VulkanSDK/1.3.296.0/macOS
+set -Ux DYLD_LIBRARY_PATH /Users/thubie/VulkanSDK/1.3.296.0/macOS/lib
+set -Ux VK_ICD_FILENAMES /Users/thubie/VulkanSDK/1.3.296.0/macOS/share/vulkan/icd.d/MoltenVK_icd.json
+set -Ux VK_LAYER_PATH /Users/thubie/VulkanSDK/1.3.296.0/macOS/share/vulkan/explicit_layer.d
+set -Ux VK_ADD_LAYER_PATH /Users/thubie/VulkanSDK/1.3.296.0/macOS/share/vulkan/explicit_layer.d/
 
-set -Ux VULKAN_SDK ~/VulkanSDK/1.3.283.0/macOS
-#set -Ux DYLD_LIBRARY_PATH ~/VulkanSDK/1.3.280.0/macOS/lib
-set -Ux VK_ICD_FILENAMES ~/VulkanSDK/1.3.280.0/macOS/share/vulkan/icd.d/MoltenVK_icd.json
-set -Ux VK_LAYER_PATH ~/VulkanSDK/1.3.280.0/macOS/share/vulkan/explicit_layer.d
+#set -Ux VK_LOADER_DEBUG driver
 
 alias ga="git add"
 alias gc="git commit"
@@ -41,4 +45,6 @@ function ls
     eza --icons=always
 end
 
-source ~/team-null-null.env
+source ~/projects/emsdk/emsdk_env.fish
+
+clear
